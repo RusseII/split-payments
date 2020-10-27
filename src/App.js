dca92a9e5ab3da7eb5b8623c9801e8b68abfe2d1
russell <russell@deephire.com>
2020-10-27 09:23:24 -0600

    add basic for peter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { Form, Input, Button, InputNumber, Typography } from "antd";
+import {useState} from 'react'
+const layout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 10 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 4, span: 10 },
+};
 
-function App() {
+const Demo = () => {
+  const [result, setResult] = useState('')
+  const onFinish = async (values) => {
+    console.log("Success:", values);
+    // const request = JSON.parse(values.paymentData);
+    const res = await fetch("/api/payments", {
+      method: "POST",
+      body: JSON.stringify(values),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.text()
+    setResult(data)
+
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Form
+    initialValues={{total: 14.5, tax: 6.5, tip: 1, fees: 0, reciept: '{"Russell": [3,3,1]}'}}
+      {...layout}
+      name='basic'
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
 
-export default App;
+      <Form.Item
+        label='Total'
+        name='total'
+        rules={[{ required: true, message: "Cant be empty" }]}
+      >
+        <InputNumber />
+      </Form.Item>
+
+      <Form.Item
+        label='Tax'
+        name='tax'
+        rules={[{ required: true, message: "Cant be empty" }]}
+      >
+        <InputNumber />
+      </Form.Item>
+
+      <Form.Item
+        label='Tip'
+        name='tip'
+        rules={[{ required: true, message: "Cant be empty" }]}
+      >
+        <InputNumber />
+      </Form.Item>
+
+      <Form.Item
+        label='Misc fees'
+        name='fees'
+        rules={[{ required: true, message: "Cant be empty" }]}
+      >
+  
+        <InputNumber />
+      </Form.Item>
+
+
+
+
+      <Form.Item
+        label='Reciept'
+        name='reciept'
+        rules={[{ required: true, message: "Cant be empty" }]}
+      >
+        <Input.TextArea />
+      </Form.Item>
+  
+      <Form.Item {...tailLayout}>
+        <Button type='primary' htmlType='submit'>
+          Submit
+        </Button>
+      </Form.Item>
+      <Typography.Text copyable>{result}</Typography.Text>
+    </Form>
+  );
+};
+
+export default Demo;
